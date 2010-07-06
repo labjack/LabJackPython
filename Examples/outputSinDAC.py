dc4252fc54f1d671586b8434c530be13eb635485
Mike Colagrosso <mike@colagrosso.net>
2010-07-06 11:20:12 -0600

    Added comment marks to header

--- Examples/outputSinDAC.py
+++ Examples/outputSinDAC.py
@@ -1,17 +1,15 @@
-"""
-" An example script to show how to output a sine wave using a DAC.
-" Because we have to do it all in software, there are limitations on how fast
-" we can update the DAC. Update intervals faster than 5 ms may give weird
-" results because of the large percentage of missed updates.
-"
-" Note: This example requires Python 2.6 to run.
-"
-" When changing the update interval and frequency, consider how your values
-" effect the waveform. A slow update interval coupled with a fast frequency
-" can result in strange behavior. Try to keep the period (1/frequency) much
-" greater than update interval.
-"
-"""
+# An example script to show how to output a sine wave using a DAC.
+# Because we have to do it all in software, there are limitations on how fast
+# we can update the DAC. Update intervals faster than 5 ms may give weird
+# results because of the large percentage of missed updates.
+#
+# Note: This example requires Python 2.6 to run.
+#
+# When changing the update interval and frequency, consider how your values
+# effect the waveform. A slow update interval coupled with a fast frequency
+# can result in strange behavior. Try to keep the period (1/frequency) much
+# greater than update interval.
+
 
 # Constants. Change these to change the results:
 
