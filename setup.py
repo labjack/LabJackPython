389cbf3b5f74df8382589c7e813bc28da2f4e927
LabJack <support@labjack.com>
2011-08-26 13:09:20 -0600

    Updates files for tagged release 8-26-2011

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='LabJackPython',
-      version='4-13-2011',
+      version='8-26-2011',
       description='The LabJack python module.',
       url='http://www.labjack.com/support/labjackpython',
       author='The LabJack crew',
