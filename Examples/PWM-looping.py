5db8e84fcf14a61ff82d976d95cfb54f201911d0
David Lopez <david@labjack.com>
2014-03-11 16:32:50 -0600

    Added U3 low-level commands to PWM-looping.py.

--- Examples/PWM-looping.py
+++ Examples/PWM-looping.py
@@ -20,17 +20,19 @@ if d.devType == 9:
     d.writeRegister(7002, 15)
 else:
     # Set the timer clock to be 4 MHz with a given divisor
-    #d.configTimerClock( TimerClockBase = 4, TimerClockDivisor = 15)
+    #d.configTimerClock( TimerClockBase = 4, TimerClockDivisor = 15) #U3 and U6
     d.writeRegister(7000, 4)
     d.writeRegister(7002, 15)
 
 # Enable the timer
-#d.configIO( NumberTimersEnabled = 1 )
+#d.configIO( NumberOfTimersEnabled = 1 )  #U3
+#d.configIO( NumberTimersEnabled = 1 )  #U6
 d.writeRegister(50501, 1)
 
 # Configure the timer for PWM, starting with a duty cycle of 0.0015%.
 baseValue = 65535
-#d.getFeedback( u6.Timer0Config(TimerMode = 0, Value = baseValue) )
+#d.getFeedback( u3.Timer0Config(TimerMode = 0, Value = baseValue) )  #U3
+#d.getFeedback( u6.Timer0Config(TimerMode = 0, Value = baseValue) )  #U6
 d.writeRegister(7100, [0, baseValue])
 
 # Loop, updating the duty cycle every time.
@@ -38,11 +40,13 @@ for i in range(65):
     currentValue = baseValue - (i * 1000)
     dutyCycle = ( float(65536 - currentValue) / 65535 ) * 100
     print "Duty Cycle = %s%%" % dutyCycle
-    #d.getFeedback( u6.Timer0( Value = currentValue, UpdateReset = True ) )
+    #d.getFeedback( u3.Timer0( Value = currentValue, UpdateReset = True ) )  #U3
+    #d.getFeedback( u6.Timer0( Value = currentValue, UpdateReset = True ) )  #U6
     d.writeRegister(7200, currentValue)
     sleep(0.3)
 print "Duty Cycle = 100%"
-#d.getFeedback( u6.Timer0( Value = 0, UpdateReset = True ) )
+#d.getFeedback( u3.Timer0( Value = 0, UpdateReset = True ) )  #U3
+#d.getFeedback( u6.Timer0( Value = 0, UpdateReset = True ) )  #U6
 d.writeRegister(7200, 0)
 
 # Close the device.
