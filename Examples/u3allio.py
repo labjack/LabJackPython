b27961c10b38e2c24aef57c2c1bf805db691c441
David Lopez <david@labjack.com>
2016-07-01 16:23:19 -0600

    Updated u3allio.py and u6allio.py for Python 3 compatibility. Added argument check. Cleaned up code. Updated u3allio.py to use calibrated analog input readings.

--- Examples/u3allio.py
+++ Examples/u3allio.py
@@ -5,66 +5,64 @@ from datetime import datetime
 
 import u3
 
-numChannels = int(sys.argv[1])
+
+try:
+    numChannels = int(sys.argv[1])
+except:
+    print("Missing or invalid integer value argument that specifies the number of analog inputs.")
+    print("Exiting.")
+    sys.exit()
 quickSample = 1
 longSettling = 0
-
 latestAinValues = [0] * numChannels
-
 numIterations = 1000
 
 d = u3.U3()
+d.getCalibrationData()
 
 try:
-    #Configure the IOs before the test starts
-    
-    FIOEIOAnalog = ( 2 ** numChannels ) - 1;
-    fios = FIOEIOAnalog & (0xFF)
-    eios = FIOEIOAnalog/256
-    
-    d.configIO( FIOAnalog = fios, EIOAnalog = eios )
-    
-    d.getFeedback(u3.PortDirWrite(Direction = [0, 0, 0], WriteMask = [0, 0, 15]))
-    
-    
+    # Configure the IOs before the test starts
+
+    FIOEIOAnalog = (2 ** numChannels) - 1
+    fios = FIOEIOAnalog & 0xFF
+    eios = FIOEIOAnalog // 256
+    d.configIO(FIOAnalog=fios, EIOAnalog=eios)
+
+    d.getFeedback(u3.PortDirWrite(Direction=[0, 0, 0], WriteMask=[0, 0, 15]))
+
     feedbackArguments = []
-    
-    feedbackArguments.append(u3.DAC0_8(Value = 125))
+    feedbackArguments.append(u3.DAC0_8(Value=125))
     feedbackArguments.append(u3.PortStateRead())
-    
-    #Check if the U3 is an HV
-    if d.configU3()['VersionInfo']&18 == 18:
+
+    # Check if the U3 is an HV
+    if d.configU3()['VersionInfo'] & 18 == 18:
         isHV = True
     else:
         isHV = False
 
     for i in range(numChannels):
-        feedbackArguments.append( u3.AIN(i, 31, QuickSample = quickSample, LongSettling = longSettling ) )
-    
-    #print feedbackArguments
-    
+        feedbackArguments.append(u3.AIN(i, 31, QuickSample=quickSample, LongSettling=longSettling))
+
     start = datetime.now()
-    # Call Feedback 1000 times
+    # Call Feedback 1000 (default) times
     i = 0
     while i < numIterations:
-        results = d.getFeedback( feedbackArguments )
-        #print results
+        results = d.getFeedback(feedbackArguments)
         for j in range(numChannels):
-            #Figure out if the channel is low or high voltage to use the correct calibration
-            if isHV == True and j < 4:
+            # Figure out if the channel is low or high voltage to use the correct calibration
+            if isHV is True and j < 4:
                 lowVoltage = False
             else:
                 lowVoltage = True
-            latestAinValues[j] = d.binaryToCalibratedAnalogVoltage(results[ 2 + j ], isLowVoltage = lowVoltage, isSingleEnded = True)
+            latestAinValues[j] = d.binaryToCalibratedAnalogVoltage(results[2 + j], isLowVoltage=lowVoltage, isSingleEnded=True)
         i += 1
 
     end = datetime.now()
     delta = end - start
-    print "Time difference: ", delta
+    print("Time difference: %s" % delta)
     dm = delta / numIterations
-    print "Time per iteration: ", dm
-    print "Time per iteration in millis: ", dm.microseconds  / 1000.0
-    print "Latest readings: ", latestAinValues
-
+    print("Time per iteration: %s" % dm)
+    print("Time per iteration in millis: %s" % (dm.microseconds / 1000.0))
+    print("Last readings: %s" % latestAinValues)
 finally:
     d.close()
--- Examples/u6allio.py
+++ Examples/u6allio.py
@@ -5,7 +5,13 @@ from datetime import datetime
 
 import u6
 
-numChannels = int(sys.argv[1])
+
+try:
+    numChannels = int(sys.argv[1])
+except:
+    print("Missing or invalid integer value argument that specifies the number of analog inputs.")
+    print("Exiting.")
+    sys.exit()
 resolutionIndex = 1
 gainIndex = 0
 settlingFactor = 0
@@ -19,42 +25,37 @@ d = u6.U6()
 d.getCalibrationData()
 
 try:
-    #Configure the IOs before the test starts
-    
-    FIOEIOAnalog = ( 2 ** numChannels ) - 1;
-    fios = FIOEIOAnalog & (0xFF)
-    eios = FIOEIOAnalog/256
-    
-    
-    d.getFeedback(u6.PortDirWrite(Direction = [0, 0, 0], WriteMask = [0, 0, 15]))
-    
-    
+    # Configure the IOs before the test starts
+
+    FIOEIOAnalog = (2 ** numChannels) - 1
+    fios = FIOEIOAnalog & 0xFF
+    eios = FIOEIOAnalog // 256
+
+    d.getFeedback(u6.PortDirWrite(Direction=[0, 0, 0], WriteMask=[0, 0, 15]))
+
     feedbackArguments = []
-    
-    feedbackArguments.append(u6.DAC0_8(Value = 125))
+
+    feedbackArguments.append(u6.DAC0_8(Value=125))
     feedbackArguments.append(u6.PortStateRead())
-    
+
     for i in range(numChannels):
-        feedbackArguments.append( u6.AIN24(i, resolutionIndex, gainIndex, settlingFactor, differential) )
-    
-    
+        feedbackArguments.append(u6.AIN24(i, resolutionIndex, gainIndex, settlingFactor, differential))
+
     start = datetime.now()
-    # Call Feedback 1000 times
+    # Call Feedback 1000 (default) times
     i = 0
     while i < numIterations:
-        results = d.getFeedback( feedbackArguments )
-        
+        results = d.getFeedback(feedbackArguments)
         for j in range(numChannels):
-            latestAinValues[j] = d.binaryToCalibratedAnalogVoltage(gainIndex, results[ 2 + j ])
+            latestAinValues[j] = d.binaryToCalibratedAnalogVoltage(gainIndex, results[2 + j])
         i += 1
 
     end = datetime.now()
     delta = end - start
-    print "Time difference: ", delta
+    print("Time difference: %s" % delta)
     dm = delta / numIterations
-    print "Time per iteration: ", dm
-    print "Time per iteration in millis: ", dm.microseconds  / 1000.0
-    print "Latest readings: ", latestAinValues
-
+    print("Time per iteration: %s" % dm)
+    print("Time per iteration in millis: %s" % (dm.microseconds / 1000.0))
+    print("Last readings: %s" % latestAinValues)
 finally:
     d.close()
