9f64171e905cf677180b44c7c249bfb7e9501c01
David Lopez <david@labjack.com>
2016-07-05 15:25:25 -0600

    Updated examples for Python 3. Cleaned up some code.

--- Examples/u3Noise.py
+++ Examples/u3Noise.py
@@ -1,7 +1,16 @@
 """
-" Name: u3Noise.py
-" Desc: ( See below )
+Name: u3Noise.py
+Desc: See DESC string.
 """
+import math
+
+try:
+    raw_input
+except NameError:  # Python 3
+    raw_input = input
+
+import u3
+
 
 DESC = """
 This program will attempt to measure the noise of a provided signal on the U3. This is good if there is ever a question of noise from the LabJack or from a signal.
@@ -14,41 +23,33 @@ Noise-Free Resolution (bits): This represents how many bits are noise free.
 
 Noise-Free Resolution (mV): The smallest value that can be represented noise-free.
 
-Connect your signal and run this program. After, connect something with an obviously low-noise signal ( like a battery ) and run the program. If you don't have a low-noise signal, you can always jumper two FIO's to Ground and measure the noise as a differential.
+Connect your signal and run this program. After, connect something with an obviously low-noise signal (like a battery) and run the program. If you don't have a low-noise signal, you can always jumper two FIOs to Ground and measure the noise as a differential.
 
 You'll find that by comparing the two results, the LabJack is rarely the reason for noisy readings.
 
 On with the test:
 """
 
-import math
 
-try:
-    raw_input
-except NameError: # Python 3
-    raw_input = input
-
-import u3
-
-def calcNoiseAndResolution(d, positiveChannel = 0, negativeChannel = 31):
+def calcNoiseAndResolution(d, positiveChannel=0, negativeChannel=31):
     readings = []
-    
-    cmd = u3.AIN(positiveChannel, negativeChannel, QuickSample = False, LongSettling = False)
-    
+
+    cmd = u3.AIN(positiveChannel, negativeChannel, QuickSample=False, LongSettling=False)
+
     for i in range(128):
-        readings.append( float(d.getFeedback(cmd)[0])/16 )
+        readings.append(float(d.getFeedback(cmd)[0]) / 16.0)
     #print readings
-    
+
     # Peak-To-Peak Noise
     p2pn = max(readings) - min(readings)
-    
+
     # Noise-free resolution in bits
     if p2pn > 0:
         nfrbits = 12 - math.log(p2pn, 2)
     else:
         nfrbits = 12
-    
-    # Noise-free resolution in milivolts
+
+    # Noise-free resolution in millivolts
     if d.deviceName.endswith("HV") and positiveChannel < 4:
         vRange = 20.6
     else:
@@ -56,31 +57,36 @@ def calcNoiseAndResolution(d, positiveChannel = 0, negativeChannel = 31):
             vRange = 4.88
         else:
             vRange = 2.44
-    
-    nfrres = ( vRange / (2**nfrbits) ) * (10 ** 3)
-    
-    return p2pn, nfrbits, nfrres
-    
-print DESC
 
-pos = raw_input("Positive Channel (0-31) [0]: ")
+    nfrres = (vRange / (2**nfrbits)) * (10 ** 3)
+
+    return p2pn, nfrbits, nfrres
+
+
+print(DESC)
+
+pos = raw_input("Positive Channel (0-15, 30, 31) [0]: ")
 try:
     pos = int(pos)
 except:
     pos = 0
 
-neg = raw_input("Negative Channel (0-31) [31]: ")
+neg = raw_input("Negative Channel (0-15, 30, 31) [31]: ")
 try:
     neg = int(neg)
 except:
     neg = 31
 
+# Open first found U3
 d = u3.U3()
+
+# Configure all FIO and EIO lines to analog inputs.
+d.configIO(FIOAnalog=0xFF, EIOAnalog=0xFF)
 
 results = calcNoiseAndResolution(d, pos, neg)
 
-print "Peak-To-Peak Noise =", results[0]
-print "Noise-Free Resolution (bits) =", results[1]
-print "Noise-Free Resolution (mV) =", results[2]
+print("Peak-To-Peak Noise = %s" % results[0])
+print("Noise-Free Resolution (bits) = %s" % results[1])
+print("Noise-Free Resolution (mV) = %s" % results[2])
 
 d.close()
--- Examples/u6Noise.py
+++ Examples/u6Noise.py
@@ -5,10 +5,11 @@ Desc: An example program that will calculate the values that can be found in
       Appendix B of the U6 User's Guide.
 """
 
-import math # Need math for square root and log.
+import math  # Need math for square root and log.
 from datetime import datetime
 
-import u6 # Import the u6 module
+import u6  # Import the u6 module
+
 
 # The size of the various ranges
 ranges = [20, 2, 0.2, 0.02]
@@ -19,36 +20,38 @@ strRanges = ["+/- 10", "+/- 1", "+/- 0.1", "+/- 0.01"]
 # Numerical versions of range that the LabJack expects
 vRanges = range(4)
 
+
 def calcNoiseAndResolution(d, resolutionIndex, voltageRange):
     """
     Takes 128 readings and calculates noise and resolution
     """
     # Make a list to hold our readings
     readings = []
-    
+
     # The feedback command to send to the device
-    cmd = u6.AIN24AR(15, ResolutionIndex = resolutionIndex, GainIndex = voltageRange, SettlingFactor = 4)
-    
+    # Analog input reading from channel 15 (GND) with settling factor 4 (200 microseconds)
+    cmd = u6.AIN24AR(15, ResolutionIndex=resolutionIndex, GainIndex=voltageRange, SettlingFactor=4)
+
     start = datetime.now()
-    
+
     # Collect 128 samples
     for i in range(128):
         readings.append(d.getFeedback(cmd)[0]['AIN'])
-    
+
     finish = datetime.now()
-    
-    print "%s per sample" % ( (finish-start) / 128)
-    
+
+    print("%s per sample" % ((finish-start) / 128))
+
     # The Peak-To-Peak Noise is difference between the max and the min.
     p2pn = max(readings) - min(readings)
-    
+
     # Noise-Free resolution in bits follows the formula:
     nfrbits = 24 - math.log(p2pn, 2)
-    
-    # Noise-Free Resolution (uV) = 
+
+    # Noise-Free Resolution (uV) =
     #                            <range> / 2 ^ < Noise-Free resolution (bits) >
-    nfrres = ( ranges[voltageRange] / (2**nfrbits) ) * (10 ** 6)
-    
+    nfrres = (ranges[voltageRange] / (2**nfrbits)) * (10 ** 6)
+
     # Get the RMS Noise by calculating the standard deviation.
     mean = sum(readings) / len(readings)
     rms = 0
@@ -56,18 +59,19 @@ def calcNoiseAndResolution(d, resolutionIndex, voltageRange):
         rms += (r - mean) ** 2
     rms = float(rms)/len(readings)
     rms = math.sqrt(rms)
-    
+
     # Effective Resolution is uses a similar formulas as Noise-Free.
     erbits = 24 - math.log(rms, 2)
-    
-    erres = ( ranges[voltageRange] / (2**erbits) ) * (10 ** 6)
-    
-    return [ p2pn, nfrbits, nfrres, rms, erbits, erres ]
-   
 
+    erres = (ranges[voltageRange] / (2**erbits)) * (10 ** 6)
+
+    return [p2pn, nfrbits, nfrres, rms, erbits, erres]
+
+
+# Open first found U6
 d = u6.U6()
 
-# If you have a U6-Pro, this will run though all the Resolution Indexs
+# If you have a U6-Pro, this will run though all the Resolution Indexes
 if d.deviceName.endswith("Pro"):
     rIndexes = range(1, 13)
 else:
@@ -75,6 +79,7 @@ else:
 
 for i in rIndexes:
     for r in vRanges:
-        rs = calcNoiseAndResolution(d,i,r)
-        print "Resolution Index = %s, Range = %s:\n\tPeak-To-Peak Noise = %s, Noise-Free Resolution (bits) = %.1f, Noise-Free Resolution (uV) = %.1f\n\tRMS Noise = %i, Effective Resolution (bits) = %.1f, Effective Resolution (uV) = %.1f\n" % (i, strRanges[r], rs[0], rs[1], rs[2], rs[3], rs[4], rs[5] )
-
+        rs = calcNoiseAndResolution(d, i, r)
+        print("Resolution Index = %s, Range = %s:" % (i, strRanges[r]))
+        print("    Peak-To-Peak Noise = %s, Noise-Free Resolution (bits) = %.1f, Noise-Free Resolution (uV) = %.1f" % (rs[0], rs[1], rs[2]))
+        print("    RMS Noise = %i, Effective Resolution (bits) = %.1f, Effective Resolution (uV) = %.1f\n" % (rs[3], rs[4], rs[5]))
